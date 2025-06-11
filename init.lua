35dbed0c4dfa8f92c613b21468b9efc2692773aa
Tatu Heikkinen <52470440+aavetatu@users.noreply.github.com>
2025-06-11 22:09:59 +0300

    Add more options to options.lua and move remaps to remap.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,15 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
-
 require("custom")
 
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system {
+	vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable",
 		lazypath,
-	}
+	})
 end
 
 -- Add lazy to the `runtimepath`, this allows us to `require` it.
--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,32 +1,31 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
-end)
-
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
+vim.opt.smartindent = true
+
 vim.opt.cursorline = true
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.opt.guicursor = ""
 
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to down window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to up window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({ higroup = "Visual", timeout = 150 })
-  end,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 150 })
+	end,
 })
-
