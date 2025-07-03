eada4febdb0fb18038a7b83146a2a80e1171916c
Tatu Heikkinen <52470440+aavetatu@users.noreply.github.com>
2025-07-03 23:59:25 +0300

    style: open neo-tree in floating window with toggle

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,28 +1,27 @@
 return {
- {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "*",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    lazy = false,
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		lazy = false,
 
-    config = function()
-    
-      require("neo-tree").setup({
-        close_if_last_window = true, 
-        window = {
-          position = "right",
-          width = 50,
-        },
-        enable_git_status = true,
-        name = {
-          use_git_status_colors = true,
-        },
-  })
-      vim.keymap.set("n", "<leader>t", "<Cmd>Neotree reveal<CR>")
-    end,
-  },
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				window = {
+					position = "float",
+					width = 50,
+				},
+				enable_git_status = true,
+				name = {
+					use_git_status_colors = true,
+				},
+			})
+			vim.keymap.set("n", "<leader>nt", "<Cmd>Neotree toggle<CR>")
+		end,
+	},
 }
