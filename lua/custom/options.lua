4db33517d1a0220f72fdde394281ca61b90cbc41
Tatu Heikkinen <52470440+aavetatu@users.noreply.github.com>
2025-07-21 23:48:20 +0300

    style: small changes to options.lua

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -7,6 +7,7 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 vim.o.textwidth = 80
+vim.opt.wrap = true
 
 vim.opt.smartindent = true
 
@@ -21,12 +22,17 @@ vim.opt.undofile = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
 end)
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank({ higroup = "Visual", timeout = 150 })
 	end,
