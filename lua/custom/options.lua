0acc6b4929f8a2a973864267ccc57874b4aa9158
Tatu Heikkinen <52470440+aavetatu@users.noreply.github.com>
2025-11-06 13:08:57 +0200

    style: decrease scrolloff to 4 from 8

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -19,7 +19,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 4
 vim.opt.signcolumn = "yes"
 
 vim.opt.ignorecase = true
